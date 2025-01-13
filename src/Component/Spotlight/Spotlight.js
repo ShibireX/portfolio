ae066d1d2ee00807eca0f7a671d3b6f24ac8f82c
Andreas Garcia <116658084+ShibireX@users.noreply.github.com>
2025-01-13 17:20:08 +0800

    feat: singapore update

--- src/Component/Spotlight/Spotlight.js
+++ src/Component/Spotlight/Spotlight.js
@@ -5,8 +5,10 @@ import "./Spotlight.css"
 import rentzPromo from "../../Asset/img/rentzPromo.png"
 import faceRecog from "../../Asset/img/faceRecog.png"
 import gaffBannerQR from "../../Asset/img/gaffBannerQR.png"
+import nusWallpaper from "../../Asset/img/nusWallpaper.png"
 
 const images = [
+    { src: nusWallpaper, title: "Currently at NUS!", description: "I am currently conducting exchange studies at the National University of Singapore, where I will sharpen my computer science and media technology skills. I look forward to meeting a lot of interesting people and broadening my cultural knowledge, whilst networking with ambitious individuals. If you are in Singapore and have any opportunities that you think would fit my expertise, feel free to reach out!", link: "https://www.nus.edu.sg/" },
     { src: gaffBannerQR, title: "Gaff has launched!", description: "Gaff is a social app I have been working on as a founding engineer for the past year. Gaff is all about changing the psychology of calling, making it easier and more fun to talk to your friends on a daily basis. In short, Gaff is the future of calling!", link: "https://apps.apple.com/se/app/gaff-talk-with-friends-daily/id1644599803" },
     { src: faceRecog, title: "Recognize your emotions!", description: "A facial expression recognition model capable of live emotion classification through web camera. The model leverages the Google FaceMesh pipeline to accurately map different points of interest on the human face to different emotions", link: "https://github.com/ShibireX/facial-expression-recognition" },
     { src: rentzPromo, title: "Listen to our music!", description: "We have been producing music under the alias Rentz for almost 10 years now. If you enjoy EDM or simply are curious about us, feel free to have a listen or two", link: "https://open.spotify.com/artist/7vzyj0u6QMnKkN60TEaLqx" },
